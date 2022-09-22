9807ff3a16e5fad07dbb57103ad66805ad7a1324
Memseletu Moundae <memseletumoundae@Memseletus-MacBook-Air.local>
2022-09-22 10:52:36 -0400

    notes

---
+++ appdevprojects/WhatsUpWorld/src/main/java/Mem.java
@@ -0,0 +1,5 @@
+public class Mem {
+    public static void main(String[] args) {
+        System.out.println("memseletu");
+    }
+}
---
+++ appdevprojects/memsproject/src/main/java/homework2.java
@@ -0,0 +1,7 @@
+public class homework2 {
+    public static void main(String[] args) {
+        //output
+        System.out.println("hello world");
+
+    }
+}
